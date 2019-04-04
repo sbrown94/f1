3c3a898a1da2ac2b306efdf4e6e3e6edf6954c7d
Scott Brown <scott.brown@steinias.com>
2019-04-04 12:01:56 +0100

    added driver standings

---
+++ src/components/DriverStandings.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="current-race">
+        <h1 class="title">CHAMPIONSHIP STANDINGS</h1>
+        <!-- <h2 class="race-title">{{raceTitle}}</h2>
+        <h2 class="circuit-name">{{circuitName}}</h2>
+        <h2 class="until-start">{{timeToRace}}</h2>
+        <div class="flag">
+          <img v-bind:src="flagSource" />
+        </div> -->
+        <div v-for="standing in standings">
+            <h2>{{standing.Driver.code}} - {{standing.points}}</h2>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'DriverStandings',
+  data: function() {
+      return {
+          standings: ''
+      }
+  },
+  created: function() {
+      this.getRaceData();
+  },
+  methods: {
+      getRaceData() {
+          this.$http.get('http://localhost:52315/api/v1/Race/GetDriverStandings').then(response => {
+            this.assignStandingsData(response.body.MRData.StandingsTable.StandingsLists[0].DriverStandings);
+          }, error => {
+              // error
+          });
+      },
+      assignStandingsData(data) {
+          this.standings = data;
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.flag {
+  width: 50%;
+  margin: 0 auto;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <CurrentRace />
+    <DriverStandings />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import CurrentRace from '@/components/CurrentRace.vue'
+import DriverStandings from '@/components/DriverStandings.vue'
 
 export default {
   name: 'home',
   components: {
-    CurrentRace
+    CurrentRace,
+    DriverStandings
   }
 }
 </script>
