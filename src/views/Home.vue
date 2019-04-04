03904cefdb7511c362c9051f4c4e46f8b9b52c5e
Scott Brown <scott.brown@steinias.com>
2019-04-04 16:13:30 +0100

    add skeleton of tab options

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="home">
     <CurrentRace />
-    <DriverStandings />
+    <DriverStandings v-if="displaying=='driver-standings'"/>
+        <div class="options">
+          <div v-on:click="switchTo('driver-standings')" class="option">Driver Standings</div>
+          <div v-on:click="switchTo('constructor-standings')" class="option">Constructor Standings</div>
+          <div v-on:click="switchTo('')" class="option">Driver Search</div>
+        </div>
   </div>
 </template>
 
@@ -15,6 +20,17 @@ export default {
   components: {
     CurrentRace,
     DriverStandings
+  },
+  data: function() {
+      return {
+          displaying: ''
+      }
+  },
+  methods: {
+    switchTo(option) {
+      console.log("change display");
+      this.displaying = option;
+    }
   }
 }
 </script>
