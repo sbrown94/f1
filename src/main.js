33dffef882aef1057ad046e09fc9ed8626f7ffe8
Scott Brown <scott.brown@steinias.com>
2019-04-05 13:46:56 +0100

    add team colors

--- src/main.js
+++ src/main.js
@@ -10,6 +10,19 @@ Vue.prototype.moment = moment
 
 Vue.config.productionTip = false
 
+Vue.prototype.teamColors = {
+  'Mercedes': '#00D2BE',
+  'Ferrari': '#DC0000',
+  'Red Bull Racing': '#1E41FF',
+  'Renault': '#FFF500',
+  'Haas': '#F0D787',
+  'Racing Point': '#F596C8',
+  'Toro Rosso': '#469BFF',
+  'McLaren': '#FF8700',
+  'Alfa Romeo Racing': '#9B0000',
+  'Williams': '#FFFFFF'
+}
+
 new Vue({
   router,
   render: h => h(App)
